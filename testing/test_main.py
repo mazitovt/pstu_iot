3e6f9a790ae1e1ea01c7e9b1da408d661136a267
antonfreides <antonamaltsev@gmail.com>
2022-01-08 23:20:00 +0500

    тесты на проверку изображений

--- testing/test_main.py
+++ testing/test_main.py
@@ -12,6 +12,14 @@ def upload_test_cases():
     dataset = [(os.path.basename(x[0]), x[2][0], open(os.path.join(x[0], x[2][0]), 'rb')) for x in get_test_cases()]
     return dataset
 
+def check_test_cases():
+    d = get_test_cases()
+    dataset = []
+    for t in d:
+        for photo in t[2]:
+            dataset.append((os.path.basename(t[0]), os.path.join(t[0], photo)))
+
+    return dataset
 
 @pytest.mark.parametrize("name,file_name,photo", upload_test_cases())
 def test_upload(name, file_name,photo):
@@ -19,3 +27,10 @@ def test_upload(name, file_name,photo):
     ar = r.json()['upload_result']
     assert f"person {name} with photo {file_name} uploaded" == ar
 
+
+@pytest.mark.parametrize("expected,photo_path", check_test_cases())
+def test_check(expected, photo_path):
+    photo = open(photo_path, 'rb')
+    r = requests.post('http://127.0.0.1:8000/checkphoto/', files={'photo': photo})
+    ar = r.json()['check_result']
+    assert f"['{expected}']" == ar
